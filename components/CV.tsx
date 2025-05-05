9be1b6ef84974104809fc25f30482caae63d3279
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-06 01:47:41 +0200

    refactor(CV): remove pointer-events-none class from embed element

--- components/CV.tsx
+++ components/CV.tsx
@@ -4,7 +4,7 @@ const CV = () => {
       <div className="border rounded shadow overflow-hidden">
         <embed
           src="/cv_manuel_fahrenholz.pdf#toolbar=0&navpanes=0&scrollbar=0"
-          className="w-full aspect-[8.5/11] min-h-[80vh] pointer-events-none"
+          className="w-full aspect-[8.5/11] min-h-[80vh]"
           type="application/pdf"
         />
       </div>
