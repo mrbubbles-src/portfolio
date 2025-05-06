7eb23a8d8ab999c89abd8b5470e750fcb6e5c2f0
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-07 01:49:36 +0200

    refactor(CV.tsx): replace embed with iframe for improved PDF rendering and responsiveness

--- components/layout/CV.tsx
+++ components/layout/CV.tsx
@@ -1,14 +1,12 @@
 const CV = () => {
   return (
-    <>
-      <div className="border rounded shadow overflow-hidden">
-        <embed
-          src="/cv_manuel_fahrenholz_view.pdf#toolbar=0&navpanes=0&scrollbar=0"
-          className="w-full aspect-[8.5/11] min-h-[80vh]"
-          type="application/pdf"
-        />
-      </div>
-    </>
+    <div className="rounded shadow overflow-hidden w-full mx-auto">
+      <iframe
+        src="/cv_manuel_fahrenholz_view.pdf#toolbar=0&navpanes=0&scrollbar=0&zoom=100&view=FitH"
+        className="w-full h-[90vh] border-none"
+        title="Curriculum Vitae"
+      />
+    </div>
   );
 };
 
