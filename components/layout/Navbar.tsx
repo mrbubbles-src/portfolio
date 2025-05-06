375183a253d63695e83f69b652f751e8c9cd9e5e
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-06 13:27:35 +0200

    refactor(Navbar): enhance accessibility and improve code readability
    - Added aria-labels and titles to navigation links for better accessibility.
    - Improved alt text for the logo image.
    - Reformatted code for consistency and clarity.

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -13,36 +13,65 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 const Navbar = () => (
   <section className="border-b shadow-sm bg-background/70 backdrop-blur sticky top-0 left-0">
     <nav className="container mx-auto px-4 py-4 flex justify-between items-center text-xl">
-      <Link href="/" className="text-xl font-bold text-primary">
+      <Link
+        href="/"
+        className="text-xl font-bold text-primary"
+        aria-label="Main navigation container">
         <Image
           src={Logo}
           width={250}
           height={50}
-          alt="mrbulles-src fullstack web developper logo"
+          alt="mrbubbles-src — Fullstack Web Developer Logo"
         />
       </Link>
-      <NavigationMenu>
+      <NavigationMenu aria-label="Main navigation" role="navigation">
         <NavigationMenuList className="gap-6">
           <NavigationMenuItem>
-            <Link href="/#about">About</Link>
+            <Link
+              href="/#about"
+              title="About section"
+              aria-label="About section">
+              About
+            </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="/#stack">Stack</Link>
+            <Link
+              href="/#stack"
+              title="Tech-stack section"
+              aria-label="Tech-stack section">
+              Stack
+            </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="/#projects">Projects</Link>
+            <Link
+              href="/#projects"
+              title="Project section"
+              aria-label="Project section">
+              Projects
+            </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="/#contact">Contact</Link>
+            <Link
+              href="/#contact"
+              title="Contact section"
+              aria-label="Contact section">
+              Contact
+            </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="/cv">CV</Link>
+            <Link
+              href="/cv"
+              title="Curriculum Vitae - Manuel Fahrenholz"
+              aria-label="Curriculum Vitae">
+              CV
+            </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <a
               href="https://github.com/mrbubbles-src"
               target="_blank"
               rel="noopener noreferrer"
+              title="GitHub - mrbubbles-src"
               aria-label="GitHub">
               <FontAwesomeIcon icon={faGithub} />
             </a>
@@ -52,6 +81,7 @@ const Navbar = () => (
               href="https://linkedin.com/in/manuel-fahrenholz"
               target="_blank"
               rel="noopener noreferrer"
+              title="LinkedIn - Manuel Fahrenholz"
               aria-label="LinkedIn">
               <FontAwesomeIcon icon={faLinkedin} />
             </a>
