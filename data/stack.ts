ef86ba7844afb644ccddddf85b1ef0e04b28034a
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-06 11:29:52 +0200

    feat(projects): define IProjects interface and update projects array
    - Introduced IProjects interface to enforce structure for project entries.
    - Updated projects array to use the new interface for better type safety.
    - Corrected a typo in the description of the 'Random Student Name Generator' project.

---
+++ data/stack.ts
@@ -0,0 +1,28 @@
+interface IStack {
+  Frontend: string[];
+  Backend: string[];
+  'Tools & Frameworks': string[];
+}
+const stack: IStack = {
+  Frontend: [
+    'HTML5',
+    'CSS3',
+    'SASS',
+    'JavaScript',
+    'TypeScript',
+    'React',
+    'TailwindCSS',
+    'Bootstrap',
+  ],
+  Backend: ['Node.js', 'Express.js', 'MongoDB', 'SQL'],
+  'Tools & Frameworks': [
+    'Next.js',
+    'Prisma',
+    'Vite',
+    'Git',
+    'Figma',
+    'Adobe Suite',
+    'Affinity Suite',
+  ],
+};
+export { stack };
