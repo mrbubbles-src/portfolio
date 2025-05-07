9892a8ed211d239964bf765acf3090b489bfa304
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-07 20:37:41 +0200

    fix(FormControl): update aria-describedby to improve accessibility
    Adjusts the aria-describedby attribute in FormControl to conditionally include form description and message IDs based on the presence of an error, enhancing screen reader support.

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -112,7 +112,11 @@ function FormControl({ ...props }: React.ComponentProps<typeof Slot>) {
     <Slot
       data-slot="form-control"
       id={formItemId}
-      aria-describedby={describedByIds || undefined}
+      aria-describedby={
+        !error
+          ? `${formDescriptionId}`
+          : `${formDescriptionId} ${formMessageId}`
+      }
       aria-invalid={!!error}
       {...props}
     />
