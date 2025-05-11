c803d05a2485a6f7d8ac5beda29a1d8a91a06096
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-11 03:28:20 +0200

    chore(about): add missing import for Link component

--- components/layout/about/about.tsx
+++ components/layout/about/about.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { getDictionary } from '@/get-digtionary';
+
 export default function About({
   dictionary,
 }: {
