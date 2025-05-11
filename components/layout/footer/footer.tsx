7fb5b6c70133690a80580f9779c70c67e6e1f615
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-11 03:28:27 +0200

    refactor(footer): replace hardcoded legal and privacy text with dictionary values for localization

--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -1,18 +1,23 @@
+import { getDictionary } from '@/get-digtionary';
 import Link from 'next/link';
 
-const Footer = () => (
+const Footer = ({
+  dictionary,
+}: {
+  dictionary: Awaited<ReturnType<typeof getDictionary>>['footer'];
+}) => (
   <footer className="mt-12 py-6 text-center text-lg text-muted-foreground border-t">
     <p>&copy; {new Date().getFullYear()} Manuel Fahrenholz</p>
     <Link
       href="/impressum"
       className="hover:text-primary focus-visible:underline transition-all duration-300 ease-in-out hover:underline underline-offset-4">
-      Impressum / Legal Notice
+      {dictionary.legal}
     </Link>{' '}
     |{' '}
     <Link
       href="/datenschutz"
       className="hover:text-primary focus-visible:underline transition-all duration-300 ease-in-out hover:underline underline-offset-4">
-      Datenschutz / Privacy Policy
+      {dictionary.privacy}
     </Link>
   </footer>
 );
